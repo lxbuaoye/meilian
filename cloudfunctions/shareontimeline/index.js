c136434b1ad5317ec3a9e14a954f8502c550264c
chenjh <us.jinhao.chen@gmail.com>
2025-07-10 01:11:54 +0800

    1.5.6 cutoff with minor fix

--- cloudfunctions/shareontimeline/index.js
+++ cloudfunctions/shareontimeline/index.js
@@ -6,7 +6,7 @@ cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }); // 使用当前云环境
 const db = cloud.database();
 const _ = db.command;
 
-const SHARE_CREDITS = 50;
+const SHARE_CREDITS = 30;
 
 // 云函数入口函数
 exports.main = async (event, context) => {
