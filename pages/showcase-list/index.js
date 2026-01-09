e8189d181bf8c52ec53a504c4528dbf2e0f4bd3c
灵犀 <zoulingxigg@gmail.com>
2026-01-09 16:06:47 +0800

    更新首页与案例展示

--- pages/showcase-list/index.js
+++ pages/showcase-list/index.js
@@ -7,12 +7,12 @@ const { CLOUD_STROAGE_PATH, CLOUD_IMAGE_BASE } = appGlobal$;
 
 Page({
   data: {
-    categoryList: ['个性定制', '工程项目'],
+    categoryList: [],
     pageLoading: false,
     showcaseList: [],
     showcaseMap: {},
     categoryIndex: 0,
-    listLoadingStatus: [0, 0],
+    listLoadingStatus: [],
     showcaseFallbackImages: [
       `${CLOUD_IMAGE_BASE}/image/common/pic@2x.png`,
       `${CLOUD_IMAGE_BASE}/image/common/pic@2x(1).png`,
@@ -40,48 +40,44 @@ Page({
   async init() {
     wx.stopPullDownRefresh();
     this.setData({ pageLoading: true });
-    const showcaseMap = {};
-    for (const category of this.data.categoryList) {
-      showcaseMap[category] = [];
+    // Load showcase-new where lx === '仿石漆' and group by allx
+    try {
+      const res = await db.collection('showcase-new').where({ lx: '仿石漆' }).get();
+      const docs = res && res.data ? res.data : [];
+      const showcaseMap = {};
+      const categorySet = new Set();
+      docs.forEach((d) => {
+        const key = d.allx || '默认';
+        categorySet.add(key);
+        if (!showcaseMap[key]) showcaseMap[key] = [];
+        // prepare cover image url from tpdz if necessary; keep tpdz raw for now
+        showcaseMap[key].push(d);
+      });
+      // normalize keys and merge duplicates (e.g., "个性案例" -> "个性定制")
+      const normalize = (k) => (k === '个性案例' ? '个性定制' : k);
+      const normalizedMap = {};
+      Object.keys(showcaseMap).forEach((k) => {
+        const nk = normalize(k);
+        if (!normalizedMap[nk]) normalizedMap[nk] = [];
+        normalizedMap[nk] = normalizedMap[nk].concat(showcaseMap[k]);
+      });
+      const categories = Object.keys(normalizedMap);
+      this._showcaseMap = normalizedMap;
+      this.setData({
+        categoryList: categories,
+        showcaseMap: normalizedMap,
+        pageLoading: false,
+        listLoadingStatus: categories.map(() => 0),
+      });
+    } catch (err) {
+      console.error('init showcase-list failed', err);
+      this.setData({ pageLoading: false });
     }
-    this._showcaseMap = showcaseMap;
-    this._loadedCategory = {};
-    this.loadShowcase();
   },
 
   async loadShowcase() {
-    const category = this.data.categoryList[this.data.categoryIndex];
-    if (this._loadedCategory && this._loadedCategory[category]) {
-      this.setData({
-        pageLoading: false,
-        [`listLoadingStatus[${this.data.categoryIndex}]`]: 0,
-      });
-      return;
-    }
-    this.setData({
-      [`listLoadingStatus[${this.data.categoryIndex}]`]: 1,
-    });
-    const { data } = await this.fetchAllShowcase(
-      this.pagination[this.data.categoryIndex].index,
-      this.pagination[this.data.categoryIndex].size,
-      this.data.categoryIndex,
-    );
-    const showcaseList = (data || []).map((item) => {
-      return {
-        ...item,
-        coverImageUrl: `${CLOUD_IMAGE_BASE}/showcase/${item._id}/cover.jpg`,
-      };
-    });
-    this.pagination[this.data.categoryIndex].index++;
-    this.seperateShowcaseByCategory(showcaseList);
-    if (this._loadedCategory) {
-      this._loadedCategory[category] = true;
-    }
-    this.setData({
-      pageLoading: false,
-      showcaseList,
-      [`listLoadingStatus[${this.data.categoryIndex}]`]: 0,
-    });
+    // With new init grouping, nothing to do here. Data already loaded.
+    this.setData({ pageLoading: false });
   },
 
   async fetchAllShowcase(index = 0, size = 4, categoryIndex = 0) {
